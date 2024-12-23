9050fdab533a0f1ffa4ac5715ffaaf3a0e445008
Ben Jones <ben@bnjns.uk>
2024-12-23 17:30:34 +0000

    feat(kubernetes): add maildev service

---
+++ configs/kubernetes/maildev.tf
@@ -0,0 +1,101 @@
+locals {
+  maildev_selector_labels = {
+    "backstage.uk/component" = "maildev"
+  }
+  maildev_labels = merge({
+    "app.kubernetes.io/name"       = "maildev"
+    "app.kubernetes.io/managed-by" = "Terraform"
+  }, local.maildev_selector_labels)
+
+  maildev_smtp_port = 25
+  maildev_web_port  = 8080
+}
+
+resource "kubernetes_service_v1" "maildev" {
+  metadata {
+    namespace   = "backstage"
+    name        = "maildev"
+    annotations = local.default_annotations
+    labels      = local.maildev_labels
+  }
+
+  spec {
+    type     = "ClusterIP"
+    selector = local.maildev_selector_labels
+
+    port {
+      name = "smtp"
+      port = local.maildev_smtp_port
+    }
+
+    port {
+      name = "web"
+      port = local.maildev_web_port
+    }
+  }
+}
+
+resource "kubernetes_stateful_set_v1" "maildev" {
+  metadata {
+    namespace   = "backstage"
+    name        = "maildev"
+    annotations = local.default_annotations
+    labels      = local.maildev_labels
+  }
+
+  spec {
+    service_name = kubernetes_service_v1.maildev.metadata[0].name
+    replicas     = 1
+
+    selector {
+      match_labels = local.maildev_selector_labels
+    }
+
+    template {
+      metadata {
+        namespace   = "backstage"
+        name        = "maildev"
+        annotations = local.default_annotations
+        labels      = local.maildev_labels
+      }
+
+      spec {
+        container {
+          name              = "maildev"
+          image             = "maildev/maildev:2.2.1"
+          image_pull_policy = "IfNotPresent"
+
+          env {
+            name  = "MAILDEV_SMTP_PORT"
+            value = tostring(local.maildev_smtp_port)
+          }
+
+          env {
+            name  = "MAILDEV_WEB_PORT"
+            value = tostring(local.maildev_web_port)
+          }
+
+          resources {
+            requests = {
+              memory = "100Mi"
+            }
+            limits = {
+              memory = "100Mi"
+            }
+          }
+        }
+
+        node_selector = {
+          "bnjns.uk/owner" = "backstage"
+        }
+
+        toleration {
+          key      = "bnjns.uk/reserved-for"
+          operator = "Equal"
+          value    = "backstage"
+          effect   = "NoSchedule"
+        }
+      }
+    }
+  }
+}
