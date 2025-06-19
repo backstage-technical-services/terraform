2af4518392594b7007d4b36629aac9776cc6068c
Ben Jones <ben@bnjns.uk>
2025-06-19 19:47:02 +0100

    fix(kubernetes): rename basic auth middleware

--- configs/kubernetes/traefik_middleware.tf
+++ configs/kubernetes/traefik_middleware.tf
@@ -1,6 +1,6 @@
-resource "kubernetes_secret" "traefik_middleware_staging_basic_auth" {
+resource "kubernetes_secret" "traefik_middleware_basic_auth" {
   metadata {
-    name        = "staging-basic-auth-users"
+    name        = "basic-auth-users"
     namespace   = kubernetes_namespace.backstage.metadata[0].name
     annotations = local.default_annotations
   }
@@ -14,18 +14,18 @@ resource "kubernetes_secret" "traefik_middleware_staging_basic_auth" {
   }
 }
 
-resource "kubernetes_manifest" "traefik_middleware_staging_basic_auth" {
+resource "kubernetes_manifest" "traefik_middleware_basic_auth" {
   manifest = {
     apiVersion = "traefik.io/v1alpha1"
     kind       = "Middleware"
     metadata = {
       namespace   = kubernetes_namespace.backstage.metadata[0].name
-      name        = "staging-basic-auth"
+      name        = "basic-auth"
       annotations = local.default_annotations
     }
     spec = {
       basicAuth = {
-        secret       = kubernetes_secret.traefik_middleware_staging_basic_auth.metadata[0].name
+        secret       = kubernetes_secret.traefik_middleware_basic_auth.metadata[0].name
         removeHeader = true
       }
     }
